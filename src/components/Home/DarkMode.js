cee9315beb8071f5b7f5347c1384227cd3bb03c1
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2024-10-22 20:55:35 +0530

    first commit

---
+++ src/components/Home/DarkMode.js
@@ -0,0 +1,8 @@
+
+  /*========== dark light mode ==========*/
+  const darkModeIcon = document.querySelector('#darkMode-icon');
+
+  darkModeIcon.onclick = () => {
+      darkModeIcon.classList.toggle('bx-sun');
+      document.body.classList.toggle('dark-mode');
+  };
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,30 @@
+import "../../App.css";
+import Nav from "../Nav";
+import "./Home.css"
+
+
+export const Home = () => {
+  return (
+    <div className="Home">
+    <nav className='navbar'>
+     <Nav/>
+    </nav>
+      <header className="App-header">
+        <div>
+          Transform Your Online Identity with Innovative and Dynamic Design
+        </div>
+        <p>
+          Let us bring your vision to life with stunning visuals that engage and
+          inspire!
+        </p>
+        <button> Explore Our Services</button>
+      </header>
+    </div>
+  );
+};
+
+
+
+ 
+
+  
