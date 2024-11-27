88624d9ec130f646f0534b12078a9a7d4ff63695
AmkitDhakad3320 <ankitdhakad3320@gmail.com>
2024-11-27 07:23:30 +0530

    css update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useRef, Suspense ,useCallback } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  Suspense,
+  useCallback,
+} from "react";
 import ScrollReveal from "scrollreveal";
 // import SwiperComponent from "../swiper";
 import { Link } from "react-router-dom";
@@ -59,7 +65,7 @@ export const Nav = React.memo(() => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: "ankitdhakad4801@gmail.com", 
+          email: "ankitdhakad4801@gmail.com",
           subject,
           question,
         }),
@@ -73,32 +79,42 @@ export const Nav = React.memo(() => {
   }, []);
 
   return (
-    <nav >
-    <div className="nav-container">
-      <div className="nav-logo">
-        <div className="logo-icon">
-          <Lottie animationData={Zx} />
+    <nav>
+      <div className="nav-container">
+        <div className="nav-logo">
+          <div className="logo-icon">
+            <Lottie animationData={Zx} />
+          </div>
+          <h2>ZXSIS</h2>
         </div>
-        <h2>ZXSIS</h2>
-      </div>
-      <div className="nav">
-        <Link to="/" className="navword" aria-label="Home">Home</Link>
-        {["About", "Services", "Portfolio", "Blog"].map((item) => (
-          <Link key={item} to={`/${item.toLowerCase()}`} className="navword" aria-label={item}>
-            {item}
+        <div className="nav">
+          <Link to="/" className="navword" aria-label="Home">
+            Home
           </Link>
-        ))}
-        <button className="quote-button" onClick={handleAskQuestion} aria-label="Get a Quote">
-          Get a Quote
-        </button>
+          {["About", "Services", "Portfolio", "Blog"].map((item) => (
+            <Link
+              key={item}
+              to={`/${item.toLowerCase()}`}
+              className="navword"
+              aria-label={item}
+            >
+              {item}
+            </Link>
+          ))}
+          <button
+            className="quote-button"
+            onClick={handleAskQuestion}
+            aria-label="Get a Quote"
+          >
+            Get a Quote
+          </button>
+        </div>
       </div>
-     
-    </div>
-    <div className="navModal">
-        <Modal 
-          className="navModal" 
-          isOpen={isModalOpen} 
-          onClose={() => setModalOpen(false)} 
+      <div className="navModal">
+        <Modal
+          className="navModal"
+          isOpen={isModalOpen}
+          onClose={() => setModalOpen(false)}
           onSend={handleSendEmail}
         />
       </div>
@@ -112,12 +128,12 @@ export const LottieComponent = React.memo(() => {
 
   return (
     <div>
-      <Lottie 
-        animationData={Bg} 
-        loop={true} 
-        autoplay={true} 
-        speed={animationSpeed} 
-        style={{ opacity: ".05" }} 
+      <Lottie
+        animationData={Bg}
+        loop={true}
+        autoplay={true}
+        speed={animationSpeed}
+        style={{ opacity: ".05" }}
       />
     </div>
   );
@@ -126,20 +142,25 @@ export const LottieComponent = React.memo(() => {
 // Header Component
 export function Header({ head, subhead, button }) {
   return (
-   
-      <header className="app-header">
-       <div className="background-container">
-      <LottieComponent />
-    </div>
-        <h1>{head}</h1>
-        <p>{subhead}</p>
-        <button>{button}</button>
-      </header>
+    <header className="app-header">
+      <div className="background-container">
+        <LottieComponent />
+      </div>
+      <h1>{head}</h1>
+      <p>{subhead}</p>
+      <button>{button}</button>
+    </header>
   );
 }
 // ===================================== ABOUTSECTION ===============================================
 
-export function AboutSection({ heading, subheading, content, imageUrl ,fallbackImage}) {
+export function AboutSection({
+  heading,
+  subheading,
+  content,
+  imageUrl,
+  fallbackImage,
+}) {
   // const fallbackImage = "https://via.placeholder.com/400";
   return (
     <div className="about-container">
@@ -152,13 +173,13 @@ export function AboutSection({ heading, subheading, content, imageUrl ,fallbackI
           <p>{content}</p>
         </div>
       </div>
-      
+
       <div className="about-image-wrapper">
         <img
           className="about-image"
           src={imageUrl || fallbackImage}
           alt="About Us"
-          loading="lazy" 
+          loading="lazy"
           width="400"
           height="auto"
         />
@@ -281,8 +302,6 @@ export const OurServices = () => {
   );
 };
 
-
-
 // ============================DATA===========================================================
 
 const portfolioData = [
@@ -312,9 +331,11 @@ export const Portfolio = () => {
         </div>
       </div>
 
-      <div className={`portfolio-card-container ${isHover ? 'paused' : ''}`}
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)} >
+      <div
+        className={`portfolio-card-container ${isHover ? "paused" : ""}`}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
         {portfolioData.map((item) => (
           <div className="portfolio-card" key={item.id}>
             <img
@@ -420,8 +441,9 @@ export const Testimonial = () => {
         "https://cdn.jsdelivr.net/npm/swiper@latest/swiper-bundle.min.js";
       script.onload = () => {
         swiperRef.current = new window.Swiper(".mySwiper", {
-          slidesPerView: 2,
+          slidesPerView: 1,
           loop: true,
+          spaceBetween: 12,
           pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -429,6 +451,11 @@ export const Testimonial = () => {
           navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
+          },
+          breakpoints: {
+            768: {
+              slidesPerView: 2,
+            },
           },
         });
 
@@ -466,44 +493,48 @@ export const Testimonial = () => {
           <div className="rule-div"></div>
         </div>
         <div className="swiper mySwiper">
-          <div className="swiper-wrapper">
+          <div className="swiper-wrapper testimonial-cards">
             {testimonials.map((testimonial) => (
               <div
                 className="swiper-slide testimonial-card"
                 key={testimonial.id}
               >
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="48"
-                    height="40"
-                    viewBox="0 0 48 40"
-                    fill="none"
-                  >
-                    <path
-                      d="M9.68264 40C7.03714 40 4.762 38.9418 2.85724 36.8254C0.95248 34.709 9.91821e-05 31.9577 9.91821e-05 28.5714C9.91821e-05 24.9735 0.793747 21.2698 2.38105 17.4603C3.86253 13.7566 5.92603 10.3704 8.57153 7.30159C11.217 4.2328 14.2329 1.79894 17.6191 0L21.4287 7.14286C18.7832 9.15344 16.6139 11.164 14.9207 13.1746C13.1218 15.1852 11.9578 17.7249 11.4287 20.7936C13.4393 20.8995 15.2911 21.9048 16.9842 23.8095C18.5715 25.8201 19.3652 27.9894 19.3652 30.3175C19.3652 31.9048 18.9419 33.4392 18.0953 34.9206C17.143 36.4021 15.926 37.619 14.4445 38.5714C12.9631 39.5238 11.3758 40 9.68264 40ZM36.0318 40C33.3863 40 31.1112 38.9418 29.2064 36.8254C27.1959 34.709 26.1906 31.9577 26.1906 28.5714C26.1906 24.9735 26.9842 21.2698 28.5715 17.4603C30.1588 13.7566 32.2752 10.3704 34.9207 7.30159C37.5662 4.2328 40.5821 1.79894 43.9683 0L47.6191 7.14286C45.0795 9.15344 42.9631 11.164 41.2699 13.1746C39.471 15.1852 38.3599 17.7249 37.9366 20.7936C39.2064 20.8995 40.4234 21.3757 41.5874 22.2222C42.7514 23.1746 43.7567 24.3915 44.6033 25.873C45.344 27.3545 45.7144 28.836 45.7144 30.3175C45.7144 31.9048 45.2911 33.4392 44.4445 34.9206C43.4922 36.4021 42.2752 37.619 40.7937 38.5714C39.3123 39.5238 37.725 40 36.0318 40Z"
-                      fill="#63C5EA"
-                    />
-                  </svg>
-                </span>
-                <div className="testimonial-card-text">
-                  <div className="testimonial-text-head">
-                    {testimonial.quote}
+                <div className="testimonial-card-content">
+                  <div className="testimonial-card-image">
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="48"
+                        height="40"
+                        viewBox="0 0 48 40"
+                        fill="none"
+                      >
+                        <path
+                          d="M9.68264 40C7.03714 40 4.762 38.9418 2.85724 36.8254C0.95248 34.709 9.91821e-05 31.9577 9.91821e-05 28.5714C9.91821e-05 24.9735 0.793747 21.2698 2.38105 17.4603C3.86253 13.7566 5.92603 10.3704 8.57153 7.30159C11.217 4.2328 14.2329 1.79894 17.6191 0L21.4287 7.14286C18.7832 9.15344 16.6139 11.164 14.9207 13.1746C13.1218 15.1852 11.9578 17.7249 11.4287 20.7936C13.4393 20.8995 15.2911 21.9048 16.9842 23.8095C18.5715 25.8201 19.3652 27.9894 19.3652 30.3175C19.3652 31.9048 18.9419 33.4392 18.0953 34.9206C17.143 36.4021 15.926 37.619 14.4445 38.5714C12.9631 39.5238 11.3758 40 9.68264 40ZM36.0318 40C33.3863 40 31.1112 38.9418 29.2064 36.8254C27.1959 34.709 26.1906 31.9577 26.1906 28.5714C26.1906 24.9735 26.9842 21.2698 28.5715 17.4603C30.1588 13.7566 32.2752 10.3704 34.9207 7.30159C37.5662 4.2328 40.5821 1.79894 43.9683 0L47.6191 7.14286C45.0795 9.15344 42.9631 11.164 41.2699 13.1746C39.471 15.1852 38.3599 17.7249 37.9366 20.7936C39.2064 20.8995 40.4234 21.3757 41.5874 22.2222C42.7514 23.1746 43.7567 24.3915 44.6033 25.873C45.344 27.3545 45.7144 28.836 45.7144 30.3175C45.7144 31.9048 45.2911 33.4392 44.4445 34.9206C43.4922 36.4021 42.2752 37.619 40.7937 38.5714C39.3123 39.5238 37.725 40 36.0318 40Z"
+                          fill="#63C5EA"
+                        />
+                      </svg>
+                    </span>
                   </div>
-                  <div className="testimonial-text-para">
-                    {testimonial.text}
-                  </div>
-                  <div className="testimonial-detail">
-                    <div
-                      className="testimonial-details-image"
-                      style={{ backgroundImage: `url(${testimonial.image})` }}
-                    ></div>
-                    <div className="testimonial-details-name">
-                      <h4>{testimonial.name}</h4>
-                      <h5>{testimonial.location}</h5>
+                  <div className="testimonial-card-text">
+                    <div className="testimonial-text-head">
+                      {testimonial.quote}
+                    </div>
+                    <div className="testimonial-text-para">
+                      {testimonial.text}
+                    </div>
+                    <div className="testimonial-detail">
+                      <div
+                        className="testimonial-details-image"
+                        style={{ backgroundImage: `url(${testimonial.image})` }}
+                      ></div>
+                      <div className="testimonial-details-name">
+                        <h4>{testimonial.name}</h4>
+                        <h5>{testimonial.location}</h5>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </div>{" "}
               </div>
             ))}
           </div>
@@ -591,28 +622,31 @@ export const Faq = () => {
   };
 
   return (
-    <div className="faq">
-      <div className="faq-head">
-        <div className="faq-heading">FAQ</div>
-        <div className="faq-text">
-          Can't find the answers you're looking for? Ask us and get answer in 24
-          hrs.
+    <>
+      <div className="faq">
+        <div className="faq-head">
+          <div className="faq-heading">FAQ</div>
+          <div className="faq-text">
+            Can't find the answers you're looking for? Ask us and get answer in
+            24 hrs.
+          </div>
+          <button className="faq-button" onClick={handleAskQuestion}>
+            Ask a question
+          </button>
         </div>
-        <button className="faq-button" onClick={handleAskQuestion}>
-          Ask a question
-        </button>
+        <div className="faq-content">
+          {faqs.map((faq, index) => (
+            <FaqCard
+              key={index}
+              question={faq.question}
+              answer={faq.answer}
+              isOpen={openIndex === index}
+              onToggle={() => handleToggle(index)}
+            />
+          ))}
+        </div>
       </div>
-      <div className="faq-content">
-        {faqs.map((faq, index) => (
-          <FaqCard
-            key={index}
-            question={faq.question}
-            answer={faq.answer}
-            isOpen={openIndex === index}
-            onToggle={() => handleToggle(index)}
-          />
-        ))}
-      </div>
+
       <div className="modal-overlay">
         <Modal
           isOpen={isModalOpen}
@@ -620,7 +654,7 @@ export const Faq = () => {
           onSend={handleSendEmail}
         />
       </div>
-    </div>
+    </>
   );
 };
 
@@ -797,7 +831,7 @@ const Modal = ({ isOpen, onClose, onSend }) => {
           </button>
         </div>
       </form>
-        {errors.submit && <p style={{ color: "green" }}>{errors.submit}</p>}
+      {errors.submit && <p style={{ color: "green" }}>{errors.submit}</p>}
     </div>
   );
 };
@@ -838,7 +872,6 @@ export const Footer = React.memo(() => {
               engage and inspire!
             </p>
           </div>
-          <div className="copyright-tag">All rights reserved by ZXSIS LLP</div>
         </div>
 
         <div className="footer-options">
@@ -853,6 +886,9 @@ export const Footer = React.memo(() => {
             <Link to="/terms-of-service">Terms of Service</Link>
             <Link to="/faq">FAQ</Link>
           </div>
+        </div>
+        <div className="foot-last">
+          <div className="copyright-tag">All rights reserved by ZXSIS LLP</div>
           <div className="footer-socialmedia">
             <a
               href="https://www.instagram.com"
@@ -931,18 +967,30 @@ export const Home = () => {
       delay: 100,
     });
 
-    sr.reveal(".services-header, .app-header, .services-card-icon, .footer-head-container, .testimonial-header", { origin: "top" });
-    sr.reveal(".home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form, .swiperArrow", { origin: "bottom" });
-    sr.reveal(".faq-head, .footer-body-left, .about-content", { origin: "left" });
-    sr.reveal(".faq-content, .footer-options, .about-image", { origin: "right" });
+    sr.reveal(
+      ".services-header, .app-header, .services-card-icon, .footer-head-container, .testimonial-header",
+      { origin: "top" }
+    );
+    sr.reveal(
+      ".home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form, .swiperArrow",
+      { origin: "bottom" }
+    );
+    sr.reveal(".faq-head,.copyright-tag , .footer-body-left, .about-content", {
+      origin: "left",
+    });
+    sr.reveal(".faq-content, .footer-options, .about-image", {
+      origin: "right",
+    });
 
     return () => sr.destroy();
   }, []);
 
   const seoData = {
     title: "Transform Your Online Identity | ZXSIS",
-    description: "We provide innovative digital design solutions to elevate your business's online presence.",
-    keywords: "web design, branding, digital marketing, graphic design, Figma, portfolio",
+    description:
+      "We provide innovative digital design solutions to elevate your business's online presence.",
+    keywords:
+      "web design, branding, digital marketing, graphic design, Figma, portfolio",
   };
 
   return (
@@ -951,7 +999,10 @@ export const Home = () => {
       <Helmet>
         <meta name="author" content="ZXSIS" />
         <meta property="og:title" content={seoData.title} />
-        <meta property="og:description" content="We design beautiful and dynamic digital experiences." />
+        <meta
+          property="og:description"
+          content="We design beautiful and dynamic digital experiences."
+        />
         <meta property="og:image" content="https://zxsis.com/og-image.jpg" />
         <meta property="og:url" content="https://zxsis.com" />
         <meta name="twitter:card" content="summary_large_image" />
@@ -968,7 +1019,8 @@ export const Home = () => {
           <Header
             head={
               <div>
-                Transform Your Online <br /> Identity with <span>Innovative</span> <br /> and <span>Dynamic</span> Design
+                Transform Your Online <br /> Identity with{" "}
+                <span>Innovative</span> <br /> and <span>Dynamic</span> Design
               </div>
             }
             subhead="Let us bring your vision to life with stunning visuals that engage and inspire!"
@@ -984,7 +1036,11 @@ export const Home = () => {
           </section>
           <section>
             <Section
-              title={<span>We design in <Symbol /> Figma</span>}
+              title={
+                <span>
+                  We design in <Symbol /> Figma
+                </span>
+              }
               subtitle="We've got you covered."
               cards={cardData}
             />
